98e75bf79782e336be149af716eed64fc6c5ad1a
JeanieFNJ <jeanie@outlook.com>
2021-06-21 19:18:52 +0000

    put questions and answers into seperate arrays

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,27 +5,31 @@ let candidateName = ""
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 //let question =input.question ("1. Who was the first American woman in space? ");
-//let correctAnswer = ('Sally Ride');
+//let correctAnswer;
 //let candidateAnswer = "";
+//let questions = []
 //let candidateAnswers= []
 
 // Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
 let questions = [
-  ["1. Who was the first American woman in space?"],
-  ["2. True or false: 5 kilometer == 5000 meters?"],
-  ["3. (5+3)/2 * 10 = ?"],
-  ["4. Given the array [8, 'Orbit', 'Trajectory' 45], what entry is at index 2?]"
-  ]
+ ["1. Who was the first American woman in space?", "Sally Ride"],
+ ["2. True or false: 5 kilometer == 5000 meters?", "True"],
+ ["3. (5+3)/2 * 10 = ?", "40"],
+ ["4. Given the array 8, 'Orbit', 'Trajectory' 45 what entry is at index 2?","Trajectory"],
+ ["What is the minimum crew size for the ISS?", "3"]
+ ]
 
 let correctAnswers = [
- ["Sally Ride"],
- ["true", "40"],
- ["Trajectory"], 
- ["3"],
- ["What is the minimum crew size for the ISS?"]
- ]
-let candidateAnswers= []
+  ["Sally Ride"],
+  ["True"],
+  ["40"],
+  ["Trajectory"],
+  ["3"]
+];
+
 // Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses.
+
+
 // Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
 // Question	Answer
 // Who was the first American woman in space?	"Sally Ride"
@@ -44,9 +48,14 @@ console.log (`${candidateName} Good Luck!`)
 //function askQuestion() {
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
 
-for (let i = 0; i < questions.length; i+=1) {
-   console.log(questions[i]);
-}
+// for (let i = 0; i < questions.length; i++) {
+//     let answer = (questions[i][0]);
+//     if (answer.toUpperCase() == questions[i][1]) {
+//         alert("Correct!");
+//         correctAnswers++;
+//     }
+//     else {
+//         alert("incorrect, the correct answer was " + questions[i][1]);
 
 // .
 // function gradeQuiz(candidateAnswers) {
