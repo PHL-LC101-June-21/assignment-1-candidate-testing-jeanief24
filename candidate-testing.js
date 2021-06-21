c3490746faebfaf3677388f71a6a0079f65b8138
JeanieFNJ <jeanie@outlook.com>
2021-06-21 00:26:07 +0000

    added arrays

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,30 @@ let candidateName = ""
 //let question =input.question ("1. Who was the first American woman in space? ");
 let correctAnswer = ('Sally Ride');
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space?",
+  "True or false: 5 kilometer == 5000 meters?",
+  "(5+3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory' 45], what entry is at index 2?"
+  ]
+
+let correctAnswers = [
+  "Sally Ride",
+  "true", "40",
+  "Trajectory", "3",
+  "What is the minimum crew size for the ISS?"
+  ]
+let candidateAnswers= []
+
+// Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
+// Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses.
+// Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
+// Question	Answer
+// Who was the first American woman in space?	"Sally Ride"
+// True or false: 5 kilometer == 5000 meters?	"true"
+// (5 + 3)/2 * 10 = ?	"40"
+// Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?	"Trajectory"
+// What is the minimum crew size for the ISS?	"3"
 
 //function askForName() {
 // TODO 1.1b: Ask for candidate's name //
@@ -24,7 +45,6 @@ console.log (`${candidateName} Good Luck!`)
 question =input.question ("1. Who was the first American woman in space? ");
 // .
 // function gradeQuiz(candidateAnswers) {
-
 
 //   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 console.log (`Candidate answer: ${question}`)
