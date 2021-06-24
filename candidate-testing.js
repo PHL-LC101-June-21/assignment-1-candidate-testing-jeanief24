b741e8a31671e538e4256c58e6aad90228329c3c
JeanieFNJ <jeanie@outlook.com>
2021-06-24 01:09:19 +0000

    Added grade and now error

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,41 +4,25 @@
 let candidateName = ""
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-//let question =input.question ("1. Who was the first American woman in space? ");
-//let correctAnswer;
-//let candidateAnswer = "";
-//let questions = []
-//let candidateAnswers= []
+let question ="1. Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride"
+let candidateAnswer = "";
+let questions = [
+ "1. Who was the first American woman in space? ", "2. True or false: 5 kilometer == 5000 meters? ", "3. (5+3)/2 * 10 = ? ", "4. Given the array 8, 'Orbit', 'Trajectory' 45 what entry is at index 2? ", "What is the minimum crew size for the ISS? " 
+]
+let correctAnswers = [
+   "Sally Ride",
+   "True", "40",
+   "Trajectory",
+   "3"
+ ]
+let candidateAnswers= []
 
 // Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
-let questions = [
- ["1. Who was the first American woman in space?", "Sally Ride"],
- ["2. True or false: 5 kilometer == 5000 meters?", "True"],
- ["3. (5+3)/2 * 10 = ?", "40"],
- ["4. Given the array 8, 'Orbit', 'Trajectory' 45 what entry is at index 2?","Trajectory"],
- ["What is the minimum crew size for the ISS?", "3"]
- ]
-let correctAnswers = 0
-let candidateAnswers = 0 //store candidate answers
-
 
 // Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses.
-for (var i = 0; i < questions.length; i++) {
-    var answer = console.log (questions[i][0]);
-    if (answer == questions[i][1]) {
-        cosole.log ("Correct " + questions[i][2]);
-        correctAnswers++;
-    }
-    else {
-        console.log ("Incorrect, the correct answer was " + questions[i][1]);
-    }
-}
-
 
 // Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
-// console.log (`"Candidate Answer: "${CandidateAnswers[i] [0]})
-// console.log (`Correct Answer: " ${CorrectAnswers [i][0]}`)
-// \n
 
 // Question	Answer
 // Who was the first American woman in space?	"Sally Ride"
@@ -47,66 +31,65 @@ for (var i = 0; i < questions.length; i++) {
 // Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?	"Trajectory"
 // What is the minimum crew size for the ISS?	"3"
 
-//function askForName() {
+function askForName() {
 // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question ("What is your name? ")
+}
 
-console.log (`Contact Name: ${candidateName}`)
-console.log (`${candidateName} Good Luck!`)
-
-//function askQuestion() {
+function askQuestion() {
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
+for (var i = 0; i < questions.length; i++) {
+  let answer = (input.question (questions[i]))
+  candidateAnswers.push (answer);
+  console.log (candidateAnswer)
+  console.log(`Your answer: ${candidateAnswers[i]}\nCorrect answer: ${correctAnswers[i]}\n`);       
+}
+let totalGrade=0
+for (let i = 0; i < correctAnswers.length; i++) {
+  let grade = (totalGrade / questions.length) * 100;
+  let candidateStatus = "";
+  if (grade >= 80) {
+    candidateStatus = "SUCCESS";
+  } else {
+    candidateStatus = "FAILED";
+  }
+  console.log(`>>> Overall Grade: ${grade}% (${totalGrade} of ${questions.length} responses correct) <<<\n>>> Status: ${candidateStatus} <<<`);
 
-// for (let i = 0; i < questions.length; i++) {
-//     let answer = (questions[i][0]);
-//     if (answer.toUpperCase() == questions[i][1]) {
-//         alert("Correct!");
-//         correctAnswers++;
-//     }
-//     else {
-//         alert("incorrect, the correct answer was " + questions[i][1]);
-
+  return grade;
+}
 // .
-// function gradeQuiz(candidateAnswers) {
+ function gradeQuiz(candidateAnswers) {
 
 //   // TODO 1.2c: 
 // Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
+let totalGrade = 0
+for (let i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      totalGrade++;
+    }
+  }
 
-conssole.log ('Candidate Answer: ${candidateAnswer} Correct Answer: ${correctAnswers}')
 
-//   let grade = "Correct"
-//   if (question == correctAnswer) {
-//     console.log ("Correct")
-//   }else {
-//    console.log ("Incorrect")
-//   }
+ }
 
-// //   return grade;
-//   if (question == correctAnswer) {
-//     console.log('Pass')
-//   }else{
-//     console.log ('Fail')
-//   }
-// }
-
-// function runProgram() {
-//   askForName();
-//   // TODO 1.1c: Ask for candidate's name //
+ function runProgram() {
+   askForName();
+   // TODO 1.1c: Ask for candidate's name //
   
-//   askQuestion();
-//   gradeQuiz(this.candidateAnswers);
-// }
+   askQuestion();
+   gradeQuiz(this.candidateAnswers);
+ }
 
 // // Don't write any code below this line //
 // // And don't change these or your program will not run as expected //
-// module.exports = {
-//   candidateName: candidateName,
-//   question: question,
-//   correctAnswer: correctAnswer,
-//   candidateAnswer: candidateAnswer,
-//   questions: questions,
-//   correctAnswers: correctAnswers,
-//   candidateAnswers: candidateAnswers,
-//   gradeQuiz: gradeQuiz,
-//   runProgram: runProgram
-// };
+ module.exports = {
+   candidateName: candidateName,
+   question: question,
+   correctAnswer: correctAnswer,
+   candidateAnswer: candidateAnswer,
+   questions: questions,
+   correctAnswers: correctAnswers,
+   candidateAnswers: candidateAnswers,
+   gradeQuiz: gradeQuiz,
+   runProgram: runProgram
+}
